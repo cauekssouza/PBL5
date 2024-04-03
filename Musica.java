76540a5c8ad27a0329495c9a08c27fe9534c1c38
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-04-03 11:05:11 -0300

    Musica.java

--- Musica.java
+++ Musica.java
@@ -13,14 +13,4 @@ public class Musica extends Spotify {
         this.genero = genero;
     }
 
-    public playlist extends Spotify{
-        private int qtMaxima;
-        private boolean aberto;
-
-        // Construtor
-        public playlist(String artista, String albumOrsingle, int duração, String playlist, int artistaMerch, String evento,String genero, int qtMaxima, boolean aberto){
-            super(artista, albumOrsingle, duração, playlist, artistaMerch, evento, qtMaxima, aberto);
-        }
-    }
-
 }
