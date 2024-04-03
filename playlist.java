3a0375c631de468eb1911dd5ba625cdc773c33e3
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-04-03 15:41:09 -0300

    playlist.java

--- playlist.java
+++ playlist.java
@@ -1,12 +1,26 @@
-public class playlist extends Spotify{
-    private int qtMaxima;
-    private boolean aberto;
+class Playlist extends Spotify {
+    private String descricao;
+    private int numeroDeMusicas;
 
-    // Construtor
-    public playlist(String artista, String albumOrsingle, int duração, String playlist, int artistaMerch, String evento,
-     int qtMaxima, boolean aberto){
-        super(artista, albumOrsingle, duração, playlist, artistaMerch, evento);
-        this.qtMaxima =qtMaxima;
-        this.aberto = aberto;
+    public Playlist(String artista, String albumOrSingle, int duracao, String playlist, int artistaMerch, String evento, String descricao, int numeroDeMusicas) {
+        super(artista, albumOrSingle, duracao, playlist, artistaMerch, evento);
+        this.descricao = descricao;
+        this.numeroDeMusicas = numeroDeMusicas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getNumeroDeMusicas() {
+        return numeroDeMusicas;
+    }
+
+    public void setNumeroDeMusicas(int numeroDeMusicas) {
+        this.numeroDeMusicas = numeroDeMusicas;
     }
 }
