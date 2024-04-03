bc6780929db6b794ad87e67d2af139490d4a10a6
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-04-03 17:10:10 -0300

    Spotify.java

--- Spotify.java
+++ Spotify.java
@@ -1,56 +1,56 @@
-public class Spotify {
+  public class Spotify {
     protected String artista;
     protected String albumOrsingle;
-    protected int duração;
+    protected String duracao;
     protected String playlist;
     protected int artistaMerch;
     protected String evento;
 
-    // Construtor
-    public Spotify(String artista, String albumOrsingle, int duração, String playlist, int artistaMerch, String evento){
+    // Constructor
+    public Spotify(String artista, String albumOrsingle, String duracao, String playlist, int artistaMerch, String evento){
         this.artista = artista;
-        this.albumOrsingle= albumOrsingle;
-        this.duração = duração;
-        this.playlist=playlist;
-        this.artistaMerch=artistaMerch;
-        this.evento=evento;
+        this.albumOrsingle = albumOrsingle;
+        this.duracao = duracao;
+        this.playlist = playlist;
+        this.artistaMerch = artistaMerch;
+        this.evento = evento;
     }
-    /* Getters ou Setters */
+
+    /* Getters and Setters */
     public String getArtista(){
         return artista;
     }
     public void setArtista(String artista){
         this.artista = artista;
     }
-    public String getalbumOrsingle(){
+    public String getAlbumOrSingle(){
         return albumOrsingle;
     }
-    public  void  setAlbumOrSingle(String albumOrsingle){
+    public void setAlbumOrSingle(String albumOrsingle){
         this.albumOrsingle = albumOrsingle;
     }
-    public int getduração(){
-        return duração;
+    public String getDuracao(){
+        return duracao;
     }
-public void setDuração(int duração){
-    this.duração = duração;
-}
-public String getPlaylist(){
-    return playlist;
-}
-public void setPlaylist(String playlist){
-    this.playlist = playlist;
-}
-public int getArtistaMerch(){
-    return artistaMerch;
-}
-public void setArtistaMerch(int artistaMerch){
-    this.artistaMerch = artistaMerch;
-}
-public String  getEvento(){
-    return evento;
-}
-public void  setEvento(String evento){
-    this.evento = evento;
-}
-
+    public void setDuracao(String duracao){
+        this.duracao = duracao;
+    }
+    public String getPlaylist(){
+        return playlist;
+    }
+    public void setPlaylist(String playlist){
+        this.playlist = playlist;
+    }
+    public int getArtistaMerch(){
+        return artistaMerch;
+    }
+    public void setArtistaMerch(int artistaMerch){
+        this.artistaMerch = artistaMerch;
+    }
+    public String getEvento(){
+        return evento;
+    }
+    public void setEvento(String evento){
+        this.evento = evento;
+    }
 }
